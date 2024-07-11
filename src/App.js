19f47fd60c4affbdd0f7bfdffd787beb88374786
akshaypan <akshaybenjamin7@gmail.com>
2024-07-11 19:51:50 +0530

    Succ

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Home from './Components/Home';
 import Addlaptop from './Components/Addlaptop';
 import Viewalllap from './Components/Viewalllap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     <Home/>
-     <Addlaptop/>
-     <Viewalllap/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/addl' element={<Addlaptop/>}/>
+      <Route path='/viewl' element={<Viewalllap/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
