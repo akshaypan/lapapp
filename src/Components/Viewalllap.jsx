2c33ae13aaa539d9b1426cf32d1d4ce583a32707
akshaypan <akshaybenjamin7@gmail.com>
2024-07-13 16:09:18 +0530

    succ

--- src/Components/Viewalllap.jsx
+++ src/Components/Viewalllap.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const Viewalllap = () => {
+  var[data,changeData]=useState(
+    [
+      {"Laptopm":"Lenovo Business S340 Laptop","Laptopcom":"Linux","price":600000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Asus TUF F-16 Laptop","Laptopcom":"intel core 15","price":800000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"REDMIBOOK 15pro Laptop","Laptopcom":"REDMI","price":105000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Asus ROG Stinx Laptop","Laptopcom":"Acer","price":156000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Lenovo Business Laptop","Laptopcom":"Linux","price":790000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Lenovo Business S360 Laptop","Laptopcom":"Linux","price":800000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Lenovo Business S390Laptop","Laptopcom":"Linux","price":1990000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"},
+      {"Laptopm":"Lenovo Business S340 Laptop","Laptopcom":"Linux","price":170000,"image":"https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg"}
+    ]
+  )
   return (
     <div>
 
@@ -10,110 +22,28 @@ const Viewalllap = () => {
       <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <div className="row g-3">
-           <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+             
+            {data.map(
+              (value,index)=>{
+                return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
+              <img src={value.image} class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
+                <h5 class="card-title">{value.Laptopm}</h5>
+                <p class="card-text">{value.Laptopcom}</p>
+                <p class="card-text">Rs.{value.price}</p>
                 <a href="#" class="btn btn-primary">Buy now</a>
               </div>
             </div>
 
           </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+              }
+            )}
 
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
 
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-            <div class="card">
-              <img src="https://images-cdn.ubuy.co.in/63534c8f63231e76f539154b-lenovo-business-s340-laptop-linux-mint.jpg" class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Lenovo Business S340 Laptop</h5>
-                <p class="card-text">Linux</p>
-                <p class="card-text">Rs.50000</p>
-                <a href="#" class="btn btn-primary">Buy now</a>
-              </div>
-            </div>
-
-            </div>
+           
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
